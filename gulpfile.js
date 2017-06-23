d60e10338ff46a9b95941c8a5e3d7b2ef86c5a3c
Tommy <tommy.zhang@cssinco.cn>
2017-06-23 15:39:55 +0800

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('default', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("*.html").on("change", browserSync.reload);
+    gulp.watch("*/*.html").on("change", browserSync.reload);
+    gulp.watch("*/*.css").on("change", browserSync.reload);
+    gulp.watch("*/*.js").on("change", browserSync.reload);
+});
